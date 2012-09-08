0dea7ff5c5d42b5377584b0be16773f2e54911ba
angiekoepplinger <angiekoepplinger@fullsail.edu>
2012-09-07 23:48:37 -0500

    Finish Javascript File

--- JS/main.js
+++ JS/main.js
@@ -6,6 +6,7 @@
 
 //Wait until the DOM is ready.
 window.addEventListener("DOMContentLoaded", function(){
+	//alert(localStorage.value(0));
 
 	//getElementById Function
 	function $(x) {
@@ -15,15 +16,15 @@ window.addEventListener("DOMContentLoaded", function(){
 
 
 	//Create select field element and populate with options.
-	function makeCats(){
+	function createList(selector, listOptions){
 		var formTag = document.getElementsByTagName("form");   //formTag is an array of all of the form tags.
-		var selectLi = $('select');
-		var makeSelect = document.createElement('select');
-			makeSelect.setAttribute("id", "groups")
+		var selectLi = $(selector);
+		var makeSelect = document.createElement("select");
+			makeSelect.setAttribute("id", "list");
 		
-		for(var i=0, j=purchaseTypeOptions.length; i<j; i++){
-			var makeOption = document.createElement('option');
-			var optText = purchaseTypeOptions[i];
+		for(var i=0, j=listOptions.length; i<j; i++){
+			var makeOption = document.createElement("option");
+			var optText = listOptions[i];
 			makeOption.setAttribute("value", optText);
 			makeOption.innerHTML = optText;
 			makeSelect.appendChild(makeOption);
@@ -32,22 +33,144 @@ window.addEventListener("DOMContentLoaded", function(){
 		selectLi.appendChild(makeSelect);
 	};
 
+	//Find value of selected radio button.
+	function getSelectedRadio(){
+		var radios = document.forms[0].asset;
+		for(var i=0; i<radios.length; i++){
+			if(radios[i].checked){
+			assetValue = radios[i].value;
+			};
+		};
+		console.log(assetValue);
+	};
 
+	function getCheckboxValue(itemName){
+		if($(itemName).checked){
+			return value = $(itemName).value;
+		} else{
+			return value = "No"
+		};
+	};
+
+	function toggleControls(x){
+		switch(x){
+			//when display data is selected, data is "on", hide form.
+			case "on":
+				$("procurementTask").style.display = "none";
+				//clear data is in an anchor tag in HTML, anchor tags are by default "inline".  
+				//If you make it a block level element, it will span the width of the display.
+				$("clearData").style.display = "inline";
+				//We do not want the displayData tab to show, because we are in this page already, so we turn it off.
+				$("displayData").style.display = "none";
+				$("addNew").style.display = "inline";
+				break;
+			case "off":
+				$("procurementTask").style.display = "block";
+				//clear data is in an anchor tag in HTML, anchor tags are by default "inline".  
+				//If you make it a block level element, it will span the width of the display.
+				$("clearData").style.display = "inline";
+				//We do not want the displayData tab to show, because we are in this page already, so we turn it off.
+				$("displayData").style.display = "inline";
+				$("addNew").style.display = "none";
+				$("items").style.display = "none";
+				break;
+			default:
+				return false;
+		};
+
+	};
+	
+	function storeData(){
+		var id = Math.floor(Math.random()*100000001);
+		//Gather up all of our form field values and store in an object.
+		//Object properties contain array with the form label and input value.
+		getSelectedRadio();
+
+		var item           			= {};
+			item.purchaseType      	= ["Purchase Type:", $("list").value];
+			item.workOrder 			= ["Work Order:", $("workOrder").value];
+			item.supportSite 		= ["Support Site:", $("supportSite").value];
+			item.itemType 			= ["Item Type:", $("list").value];
+			item.asset 				= ["Asset:", assetValue];
+			item.manufacturer 		= ["Manufacturer:", $("manufacturer").value];
+			item.partNumber 		= ["Part Number:", $("partNumber").value];
+			item.qty 				= ["Qty:", $("qty").value];
+			item.urgency 			= ["Urgency:", $("urgency").value];
+			item.mgmtApproval 		= ["Management Approval:", getCheckboxValue("mgmtApproval")];
+			item.rrRequest 			= ["R&R Request:", getCheckboxValue("rrRequest")];
+			item.dateRequired 		= ["Date Required:", $("requiredByDate").value];
+			item.completionDate		= ["Completion Date:", $("completionDate").value];
+			item.notes			 	= ["Notes:", $("notes").value];
+
+			//Save data into Local Storage: Use Stringify to convert our object to a string.
+			localStorage.setItem(id, JSON.stringify(item));
+			alert("Purchase Task Saved!");
+	};
+
+
+	function getData(){
+		toggleControls("on");
+		if(localStorage.length ===0){
+			alert("There is no data in Local Storage.")
+		};
+		//Write Data from Local Storage to the browser.
+		var makeDiv = document.createElement("div");
+		makeDiv.setAttribute("id", "items");
+		var makeUl = document.createElement("ul");
+		makeDiv.appendChild(makeUl);
+		//Loop through the local storage and grab the id(key) & associated object(value)
+		for(var i = 0, j=localStorage.length; i<j; i++){ 
+			//Safari 6 adds info to the local storage, convert strings to numbers to exclude safari storage from our data.
+			if(parseInt(localStorage.key(i)/1) === parseInt(localStorage.key(i))) {
+				var makeli = document.createElement("li");
+				makeUl.appendChild(makeli);
+				var key = localStorage.key(i);  //id associated with form submission object
+				var value = localStorage.getItem(key);  //the object, in this case: var item{}
+				//convert string from local storage back into an object using JSON.parse!
+				var object = JSON.parse(value);			
+				var makeSubUl = document.createElement("ul");
+				makeli.appendChild(makeSubUl)
+				for(var x in object){		//example: for "item.purchaseType"(key) in the object
+					var makeSubli = document.createElement("li");
+					makeSubUl.appendChild(makeSubli);
+					//get the "value" of the data in our object. The value of our key is an array.
+					//[x] = key,  [0] = label in array, [1] = value in array.
+					var objSubText = object[x][0]+ " " +object[x][1];	
+					makeSubli.innerHTML = objSubText;	
+				};
+			};	
+		};
+		document.body.appendChild(makeDiv);
+	};
+
+	function clearLocal(){
+		if(localStorage.length === 0){
+			alert("There is no data to clear.");
+		} else {
+			localStorage.clear();
+				alert("All contacts are deleted!");
+				window.location.reload();
+				return false;
+		};
+	};
 
 	//Variable defaults
-	var purchaseTypeOptions = ["Select A Type", "Maintenance", "Product"];
-	makeCats();
+	var purchaseTypeOptions = ["--Select A Type--", "Maintenance", "Product"];
+	createList("purchaseType", purchaseTypeOptions);
+	var itemTypeOptions = ["--Select A Type--", "Hardware", "Software", "Support Contract"];
+	createList("itemType", itemTypeOptions);
+	var assetValue;
+	var value;
+	// getCheckboxValue("mgmtApproval");
+	// getCheckboxValue("rrRequest");
 
-
-
-
-	//Set Link & Submit Click Events
-	// var displayData = $('displayData');
-	// displayData.addEventLister("click", getData);
-	// var clearData = $('clearData');
-	// clearData.addEventLister("click", clearLocal);
-	// var save = $('submit');
-	// submit.addEventLister('click', storeData);
+	// Set Link & Submit Click Events
+	 var displayData = $('displayData');
+	displayData.addEventListener("click", getData);
+	var clearData = $('clearData');
+	clearData.addEventListener("click", clearLocal);
+	var save = $("submit");
+	save.addEventListener("click", storeData);
 
 
 });
